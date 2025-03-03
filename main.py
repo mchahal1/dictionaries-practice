fcb4ec7bfcb121e0ce5db586d7a68643802b42e5
mchahal1 <manveerc077@deltalearns.ca>
2025-03-03 17:40:08 +0000

    a

---
+++ main.py
@@ -0,0 +1,30 @@
+# Create pizza dictionary containing a list
+# pizza = {
+# 	'crust':'thick',
+# 	'toppings':['mushrooms', 'extra cheese'],
+# 	}
+
+# # Summarize the order
+# print(f"You ordered a {pizza['crust']}-crust pizza "
+# 	"with the following toppings:") # You may continue a string like this
+
+# # Loop through the list with KEY 'toppings'
+# for topping in pizza['toppings']:
+# 	print("\t" + topping) # \t is a tab or indent
+
+
+pets = {'house':['4 doors', 'one door']}
+
+for x in pets
+    print(pets['house'])
+
+
+# favourite_languages = {
+# 	'jen':'python',
+# 	'sarah':'c',
+# 	'edward':'ruby',
+# 	'phil':'python',  # Add a comma on the last one as well
+# 	}  # This brace is indented along with the keys
+
+# for language in favourite_languages.values():
+# 	print(language.title())
